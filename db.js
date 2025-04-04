f880d2d4bcaa551d00ed6f3f6c48265ff120aa31
khushi <khushi639815@gmail.com>
2025-04-04 11:06:58 +0530

    shift mongodb to online server

--- db.js
+++ db.js
@@ -1,5 +1,11 @@
 const mongoose=require('mongoose');
-const mongoURL='mongodb://localhost:27017/hotels'
+//const mongoURL='mongodb://localhost:27017/hotels'
+// const mongoURL='mongodb+srv://khushi639815:7qoSxj141hT8quxI@cluster0.leqch1h.mongodb.net/'
+// const mongoURL='mongodb+srv://khushi639815:7qoSxj141hT8quxI@cluster0.leqch1h.mongodb.net/'
+// const mongoURL = 'mongodb+srv://khushi639815:yourPassword@cluster0.leqch1h.mongodb.net/hotels?retryWrites=true&w=majority';
+//const mongoURL='mongodb+srv://khushi639815:7qoSxj141hT8quxI@cluster0.leqch1h.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+const mongoURL = 'mongodb+srv://khushi639815:7qoSxj141hT8quxI@cluster0.leqch1h.mongodb.net/hotels?retryWrites=true&w=majority&appName=Cluster0';
+
 mongoose.connect(mongoURL,{
     useNewUrlParser:true,//new mongodb version
     useUnifiedTopology:true}) //connect
--- routes/menuRoutes.js
+++ routes/menuRoutes.js
@@ -27,5 +27,5 @@ router.get('/', async (req, res) => {
         res.status(500).json({ error: 'Internal Server Error' });
     }
 });
-
+//comment 
 module.exports = router;
